3c49ab65a86edef137ad724456d5f13a7fcb0b18
h845608 <h845608@stud.u-szeged.hu>
2023-05-06 21:31:47 +0200

    changes in the header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'home', component: HomeComponent},
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
-  {path:'about-us', component: AboutUsComponent}
+  {path:'about-us', component: AboutUsComponent},
+  {path: 'cart', component: CartComponent},
   
 ];
 
